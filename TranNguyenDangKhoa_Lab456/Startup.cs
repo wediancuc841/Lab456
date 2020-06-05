301a97968299a2ec4b1205f92c7d0c499de4baa9
Administrator <Administrator@PC254>
2020-06-05 14:01:03 +0700

    Add project files.

---
+++ TranNguyenDangKhoa_Lab456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TranNguyenDangKhoa_Lab456.Startup))]
+namespace TranNguyenDangKhoa_Lab456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
